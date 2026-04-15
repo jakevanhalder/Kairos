b61b189c16533e18574b193255b15056e2457644
jakevanhalder <jakevanhalder@gmail.com>
2026-04-15 12:53:42 -0700

    Create navbar and routes.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,25 +6,44 @@ import (
 	"net/http"
 )
 
-var templates = template.Must(template.ParseFiles(
-	"internal/web/templates/base.html",
-	"internal/web/templates/index.html",
-))
+type PageData struct {
+	ActivePage string
+}
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "base.html", nil)
+func render(w http.ResponseWriter, page string, data PageData) {
+	tmpl, err := template.ParseFiles(
+		"internal/web/templates/base.html",
+		"internal/web/templates/"+page+".html",
+	)
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err = tmpl.ExecuteTemplate(w, "base.html", data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("internal/web/static"))))
-	http.HandleFunc("/", homeHandler)
+	mux := http.NewServeMux()
+
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("internal/web/static"))))
+
+	mux.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "index", PageData{ActivePage: "dashboard"})
+	})
+	mux.HandleFunc("GET /jobs", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "jobs", PageData{ActivePage: "jobs"})
+	})
+	mux.HandleFunc("GET /history", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "history", PageData{ActivePage: "history"})
+	})
+	mux.HandleFunc("GET /logs", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "logs", PageData{ActivePage: "logs"})
+	})
 
 	log.Println("Listening on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
 	}
 }
